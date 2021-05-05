a3d876b6313184e66dc6d179b1b5acf7c3031ed8
Ernesto de la Cruz <delacruzernp@globalhitss.com>
2021-05-05 11:18:57 -0500

    primera version

---
+++ src/main/java/com/azureClaroPayAgile/soap/controller/AzureController.java
@@ -0,0 +1,28 @@
+package com.azureClaroPayAgile.soap.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.azureClaroPayAgile.soap.entity.Root;
+
+@RestController
+@RequestMapping("/rest")
+public class AzureController {
+	
+	public ResponseEntity<String> testConexion()
+		{
+			return 	new ResponseEntity<>("OK", HttpStatus.OK);
+		}
+	
+	@PostMapping("/test")
+	public ResponseEntity<String> crear(@RequestBody Root entity)
+		{
+		String a = entity.eventType;
+			//tring cadena = new String("{'respuesta':'ok'}");
+			return ResponseEntity.status(HttpStatus.CREATED).body(a);
+		}
+}
---
+++ src/main/java/com/azureClaroPayAgile/soap/entity/DetailedMessage.java
@@ -0,0 +1,7 @@
+
+package com.azureClaroPayAgile.soap.entity;
+public class DetailedMessage{
+    public String text;
+    public String html;
+    public String markdown;
+}
